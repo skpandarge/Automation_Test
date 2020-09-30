06678638eb4c7d360c5c4da6a3f2db72fd72de92
skpandarge <63003025+skpandarge@users.noreply.github.com>
2020-09-30 13:27:37 +0530

    Create seleniumtest.js

---
+++ seleniumtest.js
@@ -0,0 +1,39 @@
+There is an online shopping portal, task is to automate the flow of product purchase by following below guidelines.
+
+A. Technology - Selenium WebDriver with any of these - Java, Python, Ruby
+B. Framework (any of these) - Page object model, keyword driven, data driven 
+C. Please ensure to make use of below concepts as applicable -
+	1. Page load strategies
+	2. Single ton
+	3. Inheritance
+
+
+### Use below URL for automation
+http://automationpractice.com/index.php
+Note - Use address like below while registration
+Cleveland, OH 44113
+
+
+### Test Scenario - Log in to the portal and purchase the dress (Dresses tab), should meet below criteria -
+1. Should be 100% cotton (details of the product can be seen by click on more option on hover).
+
+ 
+
+# Additional Notes
+
+1. Feel free to reach out to us via email (`tech-hr@eduvanz.com` *or* `hr@eduvanz.com`) in case of questions.
+
+2. Please submit the solution via Github. If you're not a Github user or if you're using a private repo, [bundle your Git repo](https://git-scm.com/book/en/v2/Git-Tools-Bundling) and include it as an email attachment.
+
+3. Bonus points for unit tests and coverage.
+
+4. Bonus points for good commit messages.
+
+5. Bonus points for a scalable design.
+
+6. Data validation needs to be considered.
+
+
+   
+
+   
